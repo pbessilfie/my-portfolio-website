433e5404eb5021eaf331b78caada80d43ac7ccf1
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-06-15 19:37:40 +0000

    conditional rendering of portfolio components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
 import About from "./Components/About";
+import Blogs from "./Components/Blogs";
+import Contact from "./Components/Contact";
 import MyInfo from "./Components/MyInfo";
 import Navbar from "./Components/Navbar";
+import Resume from "./Components/Resume";
+import Works from "./Components/Works";
 
 function App() {
+  const [selectedLink, setselectedLink] = useState("About");
+  const SelectedPortDetails = () => {
+    let sl = selectedLink;
+    if (sl) {
+      switch (sl) {
+        case "Resume":
+          return <Resume />;
+        case "Works":
+          return <Works />;
+        case "Blogs":
+          return <Blogs />;
+        case "Contact":
+          return <Contact />;
+
+        default:
+          return <About />;
+      }
+    }
+    return selectedLink;
+  };
   return (
-    <main className="relative p-44 font-roboto-slab flex items-center justify-center gap-10  bg-orange-900">
+    <main className="relative p-44 font-roboto-slab flex items-center justify-center gap-10  bg-orange-900 min-h-screen">
       {/* <div className="absolute top-0 left-0 w-full h-full -z-50">
         <img src="/public/image.png" className=" h-full w-full" />
       </div> */}
@@ -13,10 +38,9 @@ function App() {
       </div>
       <div className="flex gap-20">
         <div className="min-w-96"></div>
-        <About />
-
+        {SelectedPortDetails(selectedLink)}
         <div>
-          <Navbar />
+          <Navbar setselectedLink={setselectedLink} />
         </div>
       </div>
     </main>
