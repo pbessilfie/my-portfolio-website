3439ee147333adce6847d7c8b388371c3580ffeb
Essilfie Prince Bondzie <pbessilfie001@st.ug.edu.gh>
2024-06-26 17:08:35 +0000

    added project links

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,3 +5,66 @@ export const PNL = [
   { name: "Blogs" },
   { name: "Contact" },
 ];
+
+export const projects = [
+  {
+    thumbnail: "",
+    category: "Web App",
+    projectTitle: " CineSpectre movie web app",
+  },
+  {
+    thumbnail: "",
+    category: "Web App",
+    projectTitle: " Bitblog hub - A blog website",
+  },
+  {
+    thumbnail: "",
+    category: "Web App",
+    projectTitle: " Nike Landing page clone",
+  },
+  {
+    thumbnail: "",
+    category: "Web App",
+    projectTitle: " Restaurant website",
+  },
+  {
+    thumbnail: "",
+    category: "Web App",
+    projectTitle: " FrazAi - Generative AI web app",
+  },
+  {
+    thumbnail: "",
+    category: "Web App",
+    projectTitle: "Portfolio website",
+  },
+  {
+    thumbnail: "",
+    category: "Web Design",
+    projectTitle: " CineSpectre movie web app",
+  },
+  {
+    thumbnail: "",
+    category: "Web Design",
+    projectTitle: " Bitblog hub - A blog website",
+  },
+  {
+    thumbnail: "",
+    category: "Web Design",
+    projectTitle: " Nike Landing page clone",
+  },
+  {
+    thumbnail: "",
+    category: "Web Design",
+    projectTitle: " Restaurant website",
+  },
+  {
+    thumbnail: "",
+    category: "Web Design",
+    projectTitle: " FrazAi - Generative AI web app",
+  },
+  {
+    thumbnail: "",
+    category: "Web Design",
+    projectTitle: "Portfolio website",
+  },
+];
