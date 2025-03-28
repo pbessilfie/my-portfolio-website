c08eaf489f9b6462be9f47f6940a17df275dbc64
pbessilfie <pbessilfie001@st.ug.edu.gh>
2025-03-28 22:19:26 +0000

    feat: add socialLinks constant for social media profiles

---
+++ src/constants/socialLinks.js
@@ -0,0 +1,24 @@
+
+
+ export const socialLinks = [
+    {
+      social: "Facebook",
+      color: "#206CEA",
+      link: "https://www.facebook.com/prince.essilfiebondzie",
+    },
+    {
+      social: "Twitter",
+      color: "#000",
+      link: "https://x.com/paakow_2301",
+    },
+    {
+      social: "Instagram",
+      color: "#845050",
+      link: "https://www.instagram.com/prince.essilfiebondzie/?next=%2F",
+    },
+    {
+      social: "LinkedIn",
+      color: "#2D4275",
+      link: "https://www.linkedin.com/in/prince-essilfie-bondzie-600aa322a",
+    },
+  ];
