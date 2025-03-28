8db15af8ab2eb73906ad94a427a0f21bf81a1d5f
pbessilfie <pbessilfie001@st.ug.edu.gh>
2025-03-28 22:20:02 +0000

    feat: add workExperience constant for tracking professional history

---
+++ src/constants/workExperience.js
@@ -0,0 +1,30 @@
+export const workExperience = [
+  {
+    position: "Frontend Web Engineer",
+    company: "Nestsofts",
+    location: "Accra (Remote)",
+    startDate: "July 2023",
+    endDate: "Present",
+  },
+  {
+    position: "System Administrator Intern",
+    company: "St. Luke Catholic Hospital",
+    location: "Accra",
+    startDate: "Feb 2024",
+    endDate: "May 2024",
+  },
+  {
+    position: "Co-founder & Frontend Developer",
+    company: "Ahoma",
+    location: "Accra",
+    startDate: "Feb 2024",
+    endDate: "Present",
+  },
+  {
+    position: "Frontend Developer",
+    company: "Mavritech",
+    location: "Accra",
+    startDate: "Jan 2023",
+    endDate: "Oct 2023",
+  },
+];
