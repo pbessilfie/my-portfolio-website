9e0da1fa91b232efb1ac78ad14fbeca73a1fe180
pbessilfie <pbessilfie001@st.ug.edu.gh>
2025-03-31 23:31:58 +0000

    feat: implement Education and Experience components for enhanced portfolio presentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import "./App.css";
-import MyInfo from "./Components/MyInfo";
-import Navbar from "./Components/Navbar";
-import { Outlet } from "react-router";
+import Layout from "./Components/Layout";
+import MenuContext from "./context/MenuContext";
+import ThemeProvider from "./context/ThemeContext";
+
+
+
+
 
 function App() {
   return (
-    <main className="font-roboto-slab flex items-start justify-center bg-[#f2f6f2] h-[100dvh] gap-10 pt-40 overflow-y-auto">
-      {" "}
-      <MyInfo />
-      <Outlet />
-      <Navbar />
-    </main>
+    <ThemeProvider>
+      <MenuContext>
+        <Layout />
+      </MenuContext>
+    </ThemeProvider>
   );
 }
 
