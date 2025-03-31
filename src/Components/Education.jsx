e3e78a2f29be6918a6be420160b5882696eb9d1a
pbessilfie <pbessilfie001@st.ug.edu.gh>
2025-03-31 23:34:39 +0000

    feat: add Education and Experience components for improved portfolio display

---
+++ src/Components/Education.jsx
@@ -0,0 +1,26 @@
+import { educationData } from "../constants/educationData"
+import { getIndex } from "../utils/getIndex"
+
+const Education = () => {
+    const indeces = getIndex(educationData)
+  return (
+    <div><ul className="grid grid-cols-2 gap-4">
+                {educationData.map((education, index) => (
+                  <div
+                    key={index}
+                    className={`text-sm flex flex-col gap-2 p-4  rounded-xl ${
+                      indeces.includes(index)? "bg-secondaryColor2" : "bg-secondaryColor4"
+                    }`}
+                  >
+                    <span className=" text-gray-700">{education.year}</span>
+                    <p className=" text-textColor">
+                      <span className=" font-bold">{education.certificate} -</span>{" "}
+                      {education.institution}
+                    </p>
+                  </div>
+                ))}
+              </ul></div>
+  )
+}
+
+export default Education
---
+++ src/Components/Experience.jsx
@@ -0,0 +1,31 @@
+import { workExperience } from "../constants/workExperience";
+import { getIndex } from "../utils/getIndex";
+
+const Experience = () => {
+    const indeces = getIndex(workExperience)
+  return (
+    <div>
+      <ul className="grid grid-cols-2 gap-4">
+        {workExperience.map((experience, index) => (
+          <div
+            key={index}
+            className={`text-sm flex flex-col gap-2 p-4  rounded-xl ${
+              indeces.includes(index) ? "bg-secondaryColor2" : "bg-secondaryColor4"
+            }`}
+          >
+            <span className=" text-gray-700">
+              {experience.startDate} - {experience.endDate}
+            </span>
+            <p className=" text-textColor">
+              <span className=" font-bold">{experience.position} -</span>{" "}
+              {experience.company}
+            </p>
+            <p className="text-textColor text-xs font-light">{experience.location}</p>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Experience
