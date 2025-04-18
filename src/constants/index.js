e5e708ada3f6649172576b1051e82f7037bf4d0e
pbessilfie <pbessilfie001@st.ug.edu.gh>
2025-04-18 00:18:38 +0000

    feat: update project thumbnails to use imported constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,94 +1,106 @@
+import {
+  ahomaImgSrc,
+  hcareImgSrc,
+  abAutohubImgSrc,
+  bitbloghubImgSrc,
+  nestsoftImgSrc,
+  cinespectreImgSrc,
+  abode3ImgSrc,
+  nikeLpImgSrc,
+  recipeSharingImgSrc,
+  frazaiImgSrc,
+  movieWebsiteUIImgSrc,
+  portfolioImgSrc,
+} from "./thumbnails";
+
 export const PNL = [
-  { name: "About",path:"/" },
-  { name: "Resume",path:"/resume" },
-  { name: "Works",path:"/works" },
-  { name: "Contact",path:"/contact" },
+  { name: "About", path: "/" },
+  { name: "Resume", path: "/resume" },
+  { name: "Works", path: "/works" },
+  { name: "Contact", path: "/contact" },
 ];
 
 export const projects = [
   {
-    thumbnail: "/src/assets/Aoma.png",
+    thumbnail: ahomaImgSrc,
     category: "Web App",
     projectTitle: " Ahoma Gh",
     link: "https://www.ahomagh.com/",
   },
   {
-    thumbnail: "/src/assets/hcare.png",
+    thumbnail: hcareImgSrc,
     category: "Web App",
     projectTitle: " HCare",
     link: "https://hcare.nestsofts.com/",
   },
   {
-    thumbnail: "/src/assets/Ab-autohub-motors.png",
+    thumbnail: abAutohubImgSrc,
     category: "Web App",
     projectTitle: " AB Autohub Motors",
     link: "https://hcare.nestsofts.com/",
   },
   {
-    thumbnail: "/src/assets/bitbloghub.png",
+    thumbnail: bitbloghubImgSrc,
     category: "Web App",
     projectTitle: " Bitblog hub",
     link: "https://bitbog-hub.vercel.app/",
   },
   {
-    thumbnail: "/src/assets/nestsoft.png",
+    thumbnail: nestsoftImgSrc,
     category: "Web App",
     projectTitle: "Nestsofts Company Website",
     link: "https://www.nestsofts.com/",
   },
   {
-    thumbnail: "/src/assets/cinespectre.png",
+    thumbnail: cinespectreImgSrc,
     category: "Web App",
     projectTitle: "Cinespectre",
     link: "https://abode3-site.nestsofts.com/",
   },
   {
-    thumbnail: "/src/assets/abode3.png",
+    thumbnail: abode3ImgSrc,
     category: "Web App",
     projectTitle: "Abode3 Website",
     link: "https://abode3-site.nestsofts.com/",
   },
-
   {
-    thumbnail: "/src/assets/nike-lp.png",
+    thumbnail: nikeLpImgSrc,
     category: "Web App",
     projectTitle: " Nike Landing page clone",
     link: "https://github.com/pbessilfie/Nike-LP.git",
   },
   {
-    thumbnail: "/src/assets/recipesharing.png",
+    thumbnail: recipeSharingImgSrc,
     category: "Web App",
     projectTitle: " Recipe Sharing webapp",
     link: "https://github.com/pbessilfie/My-Food-App.git",
   },
   {
-    thumbnail: "/src/assets/frazai.png",
+    thumbnail: frazaiImgSrc,
     category: "Web App",
     projectTitle: " FrazAi - Generative AI web app",
     link: "https://frazai.vercel.app/",
   },
-
   {
-    thumbnail: "/src/assets/MovieWebsiteUI.png",
+    thumbnail: movieWebsiteUIImgSrc,
     category: "Web Design",
     projectTitle: " Honey Movies",
     link: "https://www.figma.com/design/nUBwecxiWQ3TmG3JLtYT5f/Movie-website--Community-?node-id=0-1&p=f&t=XMhWOFzdxts7BkPh-0",
   },
-
   {
-    thumbnail: "/src/assets/Aoma.png",
+    thumbnail: ahomaImgSrc,
     category: "Web Design",
     projectTitle: "Ahoma Gh",
     link: "https://www.figma.com/design/NHNmJonwxLmzETEmwHxOfQ/Ahoma?node-id=1245-687&p=f&t=s35Z2Q25091GQXsA-0",
   },
   {
-    thumbnail: "/src/assets/Ab-autohub-motors.png",
+    thumbnail: abAutohubImgSrc,
     category: "Web Design",
     projectTitle: " AB Autohub Motors",
     link: "https://www.figma.com/design/8aPlBsPS9Acytbgfot1PRZ/Car-Dealership-WebApp?node-id=1-1912&t=1rMmIYEC69bnNcg4-0",
   },
   {
-    thumbnail: "/src/assets/my-portfolio.png",
+    thumbnail: portfolioImgSrc,
     category: "Web Design",
     projectTitle: "Portfolio website",
     link: "https://www.figma.com/design/OBYaa7sAp4XGt4VgaXG8jo/Portfolio?node-id=0-1&p=f&t=3FMVWOFWPXDlRu8A-0",
