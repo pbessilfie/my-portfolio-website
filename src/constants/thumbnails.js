645e53acedb2081fe6be775628aba9bf2c3ce56f
pbessilfie <pbessilfie001@st.ug.edu.gh>
2025-04-18 00:18:18 +0000

    feat: add thumbnails constants for image assets

---
+++ src/constants/thumbnails.js
@@ -0,0 +1,27 @@
+import ahomaImgSrc from "../assets/Aoma.png";
+import hcareImgSrc from "../assets/hcare.png";
+import abAutohubImgSrc from "../assets/Ab-autohub-motors.png";
+import bitbloghubImgSrc from "../assets/bitbloghub.png";
+import nestsoftImgSrc from "../assets/nestsoft.png";
+import cinespectreImgSrc from "../assets/cinespectre.png";
+import abode3ImgSrc from "../assets/abode3.png";
+import nikeLpImgSrc from "../assets/nike-lp.png";
+import recipeSharingImgSrc from "../assets/recipesharing.png";
+import frazaiImgSrc from "../assets/frazai.png";
+import movieWebsiteUIImgSrc from "../assets/MovieWebsiteUI.png";
+import portfolioImgSrc from "../assets/my-portfolio.png";
+
+export {
+  ahomaImgSrc,
+  hcareImgSrc,
+  abAutohubImgSrc,
+  bitbloghubImgSrc,
+  nestsoftImgSrc,
+  cinespectreImgSrc,
+  abode3ImgSrc,
+  nikeLpImgSrc,
+  recipeSharingImgSrc,
+  frazaiImgSrc,
+  movieWebsiteUIImgSrc,
+  portfolioImgSrc,
+};
